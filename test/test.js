2d0e8c3b5a02c785f7988118761d876743f7da59
Sérgio Ramos <mail@sergioramos.me>
2015-11-17 21:22:55 +0000

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,36 +1,36 @@
 var lab = require('lab').script();
 var assert = require('assert');
-var forceArray = require('./');
+var forceArray = require('../');
 
 
 lab.suite('force-array', function() {
   lab.test('empty', function(done) {
-    t.deepEqual(forceArray(), []);
+    assert.deepEqual(forceArray(), []);
     done();
   });
 
   lab.test('to array', function(done) {
-    t.deepEqual(forceArray(1), [1]);
+    assert.deepEqual(forceArray(1), [1]);
     done();
   });
 
   lab.test('keep array', function(done) {
-    t.deepEqual(forceArray([1]), [1]);
+    assert.deepEqual(forceArray([1]), [1]);
     done();
   });
 
   lab.test('concat array', function(done) {
-    t.deepEqual(forceArray.concat([1], [2, 3]), [1, 2, 3]);
+    assert.deepEqual(forceArray.concat([1], [2, 3]), [1, 2, 3]);
     done();
   });
 
   lab.test('concat array and number', function(done) {
-    t.deepEqual(forceArray.concat([1], 2), [1, 2]);
+    assert.deepEqual(forceArray.concat([1], 2), [1, 2]);
     done();
   });
 
   lab.test('concat numbers', function(done) {
-    t.deepEqual(forceArray.concat(1, 2), [1, 2]);
+    assert.deepEqual(forceArray.concat(1, 2), [1, 2]);
     done();
   });
 });
